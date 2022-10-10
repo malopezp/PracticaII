6c6f5c5af78603f21a350866f25f9504b070dd7d
malopezp <malopezp@192.168.1.138>
2022-10-09 23:13:23 -0600

    Practica 02

---
+++ src/main/java/com/PracticaII/PracticaIiApplication.java
@@ -0,0 +1,13 @@
+package com.PracticaII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PracticaII/PracticaIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PracticaII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
